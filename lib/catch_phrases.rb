d1c374556eb6605e427cc38850df5f5f028e3eae
Scott Hoisington <hoyzington@gmail.com>
2019-10-29 04:18:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,10 @@
 def mario
-  phrase = "It's-a me, Mario!"
+  phrase = "It's-a me, Mario!\n"
   puts phrase
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "Thank You Mario! But Our Princess Is In Another Castle!\n"
   puts status
 end
+
